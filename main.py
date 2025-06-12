51eb32600fb786f450e086d2c7859df22cf00b0c
Gasti Paschetta <gpaschetta17@gmail.com>
2025-06-12 19:26:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-# Punto de entrada, contiene la función principal y el menú interactivo
+from model import Persona
+from functions import conjuntos
+from utils import mock
+
+personas = mock.crearPersonas()
+#for persona in personas:
+#    print(persona)
+
+conjuntosDNI = []
+
+for persona in personas:
+    conjDNI = conjuntos.obtenerConjuntoDNI(persona.dni)
+    conjuntosDNI.append(conjDNI)
+
+def operaciones_conjuntos(conjuntosDNI):
+    if not conjuntosDNI:
+        return set(), set(), set(), set()
+
+    union = set.union(*conjuntosDNI) # UNION
+    interseccion = set.intersection(*conjuntosDNI) # INTERSECCION
+    
+    # DIFERENCIA
+    # Copia el valor del primer conjunto y resta desde el siguiente en la lista hasta el ultimo.
+    diferencia = conjuntosDNI[0].copy()
+    for conjunto in conjuntosDNI[1:]:
+        diferencia -= conjunto
+
+    # DIFERENCIA SIMETRICA
+    diferencia_sim = conjuntosDNI[0].copy()
+    for conjunto in conjuntosDNI[1:]:
+        diferencia_sim = conjuntosDNI[0].symmetric_difference(conjunto)
+
+    return union, interseccion, diferencia, diferencia_sim
+
+print(operaciones_conjuntos(conjuntosDNI))
