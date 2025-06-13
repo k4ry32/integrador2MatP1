596668b10256f4a006b49d159d752a2e22a74b70
k4ry32 <karinavrodriguez1609@gmail.com>
2025-06-13 14:54:54 -0300

    funcion para obtener el producto cartesiano

--- functions/conjuntos.py
+++ functions/conjuntos.py
@@ -4,12 +4,16 @@
 def obtenerConjuntoDNI(dni):
     return set(dni)
 
+# Funcion para operaciones con conjuntos (UNION, INTERSECCION, DIFERENCIA, DIFERENCIA SIMETRICA)
 def operaciones_conjuntos(conjuntosDNI):
     if not conjuntosDNI:
         return set(), set(), set(), set()
 
-    union = set.union(*conjuntosDNI) # UNION
-    interseccion = set.intersection(*conjuntosDNI) # INTERSECCION
+    # UNION
+    union = set.union(*conjuntosDNI) 
+
+    # INTERSECCION
+    interseccion = set.intersection(*conjuntosDNI) 
     
     # DIFERENCIA
     # Copia el valor del primer conjunto y resta desde el siguiente en la lista hasta el ultimo.
@@ -23,3 +27,16 @@ def operaciones_conjuntos(conjuntosDNI):
         diferencia_sim = conjuntosDNI[0].symmetric_difference(conjunto)
 
     return union, interseccion, diferencia, diferencia_sim
+
+
+# Funcion para obtener el producto cartesiano entre los conjuntos de años y edades
+def obtenerProductoCartesiano(conjunto1, conjunto2):
+    nuevoConjunto = []
+
+    # Se recorre el conjunto 1, por cada elemento se lo une con los elementos del conjunto 2
+    for x in conjunto1:
+        for y in conjunto2:
+            newTuple = (x, y)
+            nuevoConjunto.append(newTuple)
+
+    return nuevoConjunto
