b5e1194855260fe7001b2d9a45f970487a766b6c
Gasti Paschetta <gpaschetta17@gmail.com>
2025-06-13 11:38:01 -0300

    ajustes

--- main.py
+++ main.py
@@ -11,25 +11,4 @@ for persona in personas:
     conjDNI = conjuntos.obtenerConjuntoDNI(persona.dni)
     conjuntosDNI.append(conjDNI)
 
-def operaciones_conjuntos(conjuntosDNI):
-    if not conjuntosDNI:
-        return set(), set(), set(), set()
-
-    union = set.union(*conjuntosDNI) # UNION
-    interseccion = set.intersection(*conjuntosDNI) # INTERSECCION
-    
-    # DIFERENCIA
-    # Copia el valor del primer conjunto y resta desde el siguiente en la lista hasta el ultimo.
-    diferencia = conjuntosDNI[0].copy()
-    for conjunto in conjuntosDNI[1:]:
-        diferencia -= conjunto
-
-    # DIFERENCIA SIMETRICA
-    diferencia_sim = conjuntosDNI[0].copy()
-    for conjunto in conjuntosDNI[1:]:
-        diferencia_sim = conjuntosDNI[0].symmetric_difference(conjunto)
-
-    return union, interseccion, diferencia, diferencia_sim
-
-# ¡¡¡ VERIFICA QUE FUNCIONA, BORRAR PARA LA ENTREGA !!!
 print(operaciones_conjuntos(conjuntosDNI))
