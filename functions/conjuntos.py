7b821a22557ed0032e23d6b2cf0e784f88ba3c5b
Gasti Paschetta <gpaschetta17@gmail.com>
2025-06-13 17:25:04 -0300

    Update conjuntos.py

--- functions/conjuntos.py
+++ functions/conjuntos.py
@@ -4,22 +4,35 @@
 def obtenerConjuntoDNI(dni):
     return set(dni)
 
+<<<<<<< Updated upstream
 def operaciones_conjuntos(conjuntosDNI):
     if not conjuntosDNI:
         return set(), set(), set(), set()
 
     union = set.union(*conjuntosDNI) # UNION
     interseccion = set.intersection(*conjuntosDNI) # INTERSECCION
+=======
+# Funcion para operaciones con conjuntos (UNION, INTERSECCION, DIFERENCIA, DIFERENCIA SIMETRICA)
+def operaciones_conjuntos(dnis):
+    if not dnis:
+        return set(), set(), set(), set()
+
+    # UNION
+    union = set.union(*dnis) 
+
+    # INTERSECCION
+    interseccion = set.intersection(*dnis) 
+>>>>>>> Stashed changes
     
     # DIFERENCIA
     # Copia el valor del primer conjunto y resta desde el siguiente en la lista hasta el ultimo.
-    diferencia = conjuntosDNI[0].copy()
-    for conjunto in conjuntosDNI[1:]:
+    diferencia = dnis[0].copy()
+    for conjunto in dnis[1:]:
         diferencia -= conjunto
 
     # DIFERENCIA SIMETRICA
-    diferencia_sim = conjuntosDNI[0].copy()
-    for conjunto in conjuntosDNI[1:]:
-        diferencia_sim = conjuntosDNI[0].symmetric_difference(conjunto)
+    diferencia_sim = dnis[0].copy()
+    for conjunto in dnis[1:]:
+        diferencia_sim = dnis[0].symmetric_difference(conjunto)
 
     return union, interseccion, diferencia, diferencia_sim
