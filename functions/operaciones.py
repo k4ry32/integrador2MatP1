bb52553b0f49ef885f5f446d099ceb2d5d224b1a
Javier Ovelar <isaiasjovelar@gmail.com>
2025-06-13 22:05:33 -0300

    Corregida la condenada funcion

--- functions/operaciones.py
+++ functions/operaciones.py
@@ -35,7 +35,6 @@ def grupoPar(conjunto1,conjunto2,conjunto3,conjunto4,conjunto5):
                 lista = list(conjunto)
                 lista = [int(i) for i in lista]
                 if sum(lista)%2==0:
-
                         par+=1
                 else:
                        impar+=1
